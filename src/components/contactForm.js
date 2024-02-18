91e211bdb7f3c8894102ae675759a12fed172f5d
Alexis Jimenez <Alexis.jm29@gmail.com>
2024-02-17 22:01:08 -0400

    first commit

---
+++ src/components/contactForm.js
@@ -0,0 +1,77 @@
+import images from '../images/exporting.js';
+import { useState } from 'react';
+import Modal from './modal.js';
+
+const Contact = () => {
+
+    let urlAPI = process.env.REACT_APP_API_URL;
+    console.log(urlAPI)
+
+    const [formvalue, setFormvalue] = useState({email:'', name:'', body: ''})
+    const [show, setShow] = useState(false)
+
+    const handleimput = (e) => {
+        const {name, value} = e.target;
+        setFormvalue({...formvalue, [name]:value})
+    }
+
+    const getCookie = (name) => {
+        let cookie = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+        return cookie ? cookie[2] : null;
+    }
+
+    const handlesubmit = (e) => {
+        e.preventDefault();
+        let message = `You have received an email from a future client
+    - Name: ${formvalue.name}
+    - Email: ${formvalue.email}
+    
+    - Message: ${formvalue.body}`;
+
+        fetch(`${urlAPI}/api/contact`, {
+            method: 'POST',
+            headers:{
+                'Content-Type':'application/json',
+                "X-CSRFToken": getCookie("csrftoken"),
+            },
+            body: JSON.stringify({
+                message
+            })
+        })
+        .then(response => response.json())
+        .then(result => {
+            console.log(result);
+        });
+
+        document.getElementById('name').value = '';
+        document.getElementById('email').value = '';
+        document.getElementById('body').value = '';
+          
+        setShow(true)
+        
+        setTimeout(() => {
+            setShow(false)
+        }, 3000)
+
+        
+    }
+
+
+    return(
+        <div id='contact' className='section-container flex-col align-center section-margin section-padding' style={{backgroundImage: `url(${images.background})`, backgroundSize: 'cover'}}>
+            <h4 className='skills-title'>Contact me</h4>
+            <form className='contact-form flex-col section-padding' onSubmit={handlesubmit}>
+                <input name='name' onChange={handleimput} value={formvalue.name} id='name' type='text' placeholder='Your name' className='contact-input' required></input>
+                <input name='email' onChange={handleimput}  value={formvalue.email}  id='email' type='text' placeholder='Your email' className='contact-input' required></input>
+                <div className='flex-col'>
+                    <label className='message-label'>Your message</label>
+                    <textarea id='body' name='body' onChange={handleimput}  value={formvalue.body} className='message' required></textarea>
+                </div>
+                <input type='submit' className='submit' value='Submit'></input>
+            </form>
+            <Modal open={show} content={'Mail successfully sent'}/>
+        </div>
+    )
+}
+
+export default Contact
